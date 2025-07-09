8ced2e37e51b905179cb82c413c313296e55a957
kyh174 <kyh174@naver.com>
2025-07-09 20:27:18 +0900

    vite.config.ts 수정
    base 추가
    build 시 base 변경

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import commonjs from "@rollup/plugin-commonjs"; // Import the plugin
 
-export default defineConfig({
-  plugins: [commonjs({ sourceMap: true }), sveltekit()],
+export default defineConfig(({ mode, command }) => {
+  const isBuild = command === "build";
+  return {
+    plugins: [commonjs({ sourceMap: true }), sveltekit()],
+    base: isBuild ? "/editor/arduino" : "/",
+  };
 });
